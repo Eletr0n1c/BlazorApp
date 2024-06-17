ee2a31cdecff03c6b7900446f019125a432a23db
Eletr0n1c <danielmoraessid@gmail.com>
2024-06-17 12:17:08 -0300

    first commit

---
+++ BlazorApp/Reponses/CriarUsuarioResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp.Reponses
+{
+    public class CriarUsuarioResponse
+    {
+        public string? Message { get; set; }
+        public CriarResponse? Response { get; set; }
+    }
+
+    public class CriarResponse
+    {
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DataHoraCadastro { get; set; }
+    }
+
+}
---
+++ BlazorApp/ViewModels/AutenticarUsuarioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.ViewModels
+{
+    public class AutenticarUsuarioViewModel
+    {
+        [Required(ErrorMessage ="Por favor, informe o email de acesso.")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido")]
+        public string? Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?=.*[^\da-zA-Z]).{8,}$",
+            ErrorMessage = "Por favor, informe a senha com letras maiúsculas, minúsculas, números, símbolos e pelo menos 8 caracteres.")]
+        [Required(ErrorMessage ="Por favor, informe a senha de acesso.")]
+        public string? Senha { get; set; }
+    }
+}
